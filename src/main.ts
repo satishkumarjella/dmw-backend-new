91aef44dbabc476fc923502c18c84627c1cde898
satishkumarjella <satishkumarjella@gmail.com>
2026-06-18 13:33:38 +0530

    prod

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  // Add API prefix so routes like /api/auth/login map to controllers
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
