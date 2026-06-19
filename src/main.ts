6dfe26972a81eff368cdd6786cbeef63ff82964a
satishkumarjella <satishkumarjella@gmail.com>
2026-06-19 16:46:13 +0530

    prod

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // Add API prefix so routes like /api/auth/login map to controllers
-  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
